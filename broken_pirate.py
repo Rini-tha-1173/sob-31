fa0a843f26d26c21f1dded6a417138993986e219
rinit <rr1173@live.mdx.ac.uk>
2025-03-01 23:52:14 +0400

    RR: adding the quote RR: changing the parenthesis to square bracket RR: change elif to else and add ':' RR: add indentation

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,5 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
+greeting = input("Hello, possible pirate! What's the password?") # RR: adding the quote
+if greeting in ["Arrr!"]: # RR: changing the parenthesis to square bracket
 	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+else: # RR: change elif to else and add ':'
+	print("Greetings, hater of pirates!") # RR: add indentation
